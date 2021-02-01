5c3aeb3ce824befaf34e1200a0210900c6fb6107
john_wisneski <john.j.wisneski@gmail.com>
2021-02-01 05:02:12 -0500

    commit tree b6e3a4900e7607cf735c3c8bbd27c0310228826a

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+function writeCards(array,event) {
+    let thankYouCards = []
+    for (let i=0; i < array.length; i++) {
+        thankYouCards.push(`Thank you, ${array[i]}, for the wonderful surprise gift!`);
+    }
+    return thankYouCards
+}
+
+function countDown(int) {
+    while (int > 0) {
+        console.log(int);
+        int -= 1;
+    }
+    console.log(int);
+}
